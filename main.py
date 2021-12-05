f6e28b94247c1791668a140a5cc4f5f307b969fc
ngeks <ngeksdev@gmail.com>
2021-12-06 03:13:00 +0800

    Fetch questions from API.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from ui import UI
+from question import Question
+
+
+def fetch_trivia_data():
+    """Fetch trivia questions using API."""
+    import requests
+
+    params = {
+        'amount': 25,
+        'type': "boolean"
+    }
+    resp = requests.get('https://opentdb.com/api.php', params=params)
+    return resp.json()['results']
+
 
 if __name__ == '__main__':
+    questions = [Question(data['question'], data['correct_answer']) for data in fetch_trivia_data()]
     UI()
+    

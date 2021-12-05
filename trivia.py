5c5ca5c5e482f4652eb8b16edd68ed0bd88e4f57
ngeks <ngeksdev@gmail.com>
2021-12-06 03:31:09 +0800

    Refactor Trivia class.

--- trivia.py
+++ trivia.py
@@ -3,16 +3,16 @@ class Trivia:
         self.question_number = 0
         self.score = 0
         self.questions = questions
-        self.current_question = None
+        self.current = None
 
     def get_new_question(self):
         import html
-        self.current_question = self.questions[self.question_number]
+        self.current = self.questions[self.question_number]
         self.question_number += 1
-        return html.unescape(self.current_question.text)
+        return html.unescape(self.current.question)
 
     def verify_answer(self, answer_given):
-        correct_answer = self.current_question.answer
+        correct_answer = self.current.answer
 
         if answer_given == correct_answer:
             self.score += 1
